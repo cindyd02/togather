624c3336982f12a6b239f02d4cd496e8dec93e26
cindyd02 <127247829+cindyd02@users.noreply.github.com>
2025-02-22 13:21:15 -0500

    link home to sign in

--- src/frontend/Home/Home.js
+++ src/frontend/Home/Home.js
@@ -1,21 +1,30 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Home.css";
 
+
 const Home = () => {
-  return (
-    <div className="home-container">
-      <h1 className="typewriter">
-        <span>Let's Explore toGather!</span>
-      </h1>
-      <p>
-        New to the city? Join us to find events near you and connect with others who want to explore too!
-      </p>
-      <button onClick={() => alert("Login logic goes here")}>Login</button>
-      <p2>
-        Don't have an account? <a href="/sign-in">Sign up</a>
-      </p2>
-    </div>
-  );
-};
+    const navigate = useNavigate();
+  
+    const handleLogin = () => {
+      navigate('/sign-in');
+    };
+  
+    return (
+      <div className="home-container">
+        <h1 className="typewriter">
+          <span>Let's Explore toGather!</span>
+        </h1>
+        <p>
+          New to the city? Join us to find events near you and connect with others who want to explore too!
+        </p>
+        <button onClick={handleLogin}>Login</button>
+        <p2>
+          Don't have an account? <Link to="/sign-in">Sign up</Link>
+        </p2>
+      </div>
+    );
+  };
+  
 
 export default Home;
